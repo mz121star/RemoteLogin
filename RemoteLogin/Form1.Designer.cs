cfe0d1aa1619e9c949881b6d97dd162a8bab59b8
mz121star <mz121star@gmail.com>
2014-02-19 17:55:55 +0800

    UDP Remote Login

--- RemoteLogin/Form1.Designer.cs
+++ RemoteLogin/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.btn_cookie = new System.Windows.Forms.Button();
+            this.btn_rcookie = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txt_Cookie
@@ -47,11 +48,11 @@
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.button1.Location = new System.Drawing.Point(415, 584);
+            this.button1.Location = new System.Drawing.Point(395, 457);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(117, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "SEND";
+            this.button1.Text = "SEND——>Server";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -63,6 +64,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(376, 150);
             this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "{\"from\":\"192.168.112.11:33333\",\"flag\":\"0\",\"url\":\"http://www.weibo.com\"}";
             // 
             // webBrowser1
             // 
@@ -78,7 +80,8 @@
             // 
             // btn_cookie
             // 
-            this.btn_cookie.Location = new System.Drawing.Point(415, 552);
+            this.btn_cookie.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btn_cookie.Location = new System.Drawing.Point(414, 504);
             this.btn_cookie.Name = "btn_cookie";
             this.btn_cookie.Size = new System.Drawing.Size(75, 23);
             this.btn_cookie.TabIndex = 4;
@@ -86,11 +89,21 @@
             this.btn_cookie.UseVisualStyleBackColor = true;
             this.btn_cookie.Click += new System.EventHandler(this.btn_cookie_Click);
             // 
+            // btn_rcookie
+            // 
+            this.btn_rcookie.Location = new System.Drawing.Point(396, 574);
+            this.btn_rcookie.Name = "btn_rcookie";
+            this.btn_rcookie.Size = new System.Drawing.Size(93, 23);
+            this.btn_rcookie.TabIndex = 5;
+            this.btn_rcookie.Text = "<-Return Cookie";
+            this.btn_rcookie.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(982, 619);
+            this.Controls.Add(this.btn_rcookie);
             this.Controls.Add(this.btn_cookie);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.textBox2);
@@ -111,6 +124,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Button btn_cookie;
+        private System.Windows.Forms.Button btn_rcookie;
 
     }
 }
